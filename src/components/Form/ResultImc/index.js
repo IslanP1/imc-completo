2d1dad545acb9461b3500cfb7da7e5de3cd178d5
IslanP1 <islanpereiradeoliveira@gmail.com>
2022-12-16 14:23:47 -0300

    Styleish

--- src/components/Form/ResultImc/index.js
+++ src/components/Form/ResultImc/index.js
@@ -1,12 +1,52 @@
 import React from "react";
-import { View, Text} from "react-native";
+import { View, Text, StyleSheet } from "react-native";
+import { Button } from "react-native-paper";
 
-export default function ResultImc(props){
-    return(
-        <View>
-            <Text>{props.messageResultImc}</Text>
-            <Text>{props.resultImc}</Text>
+export default function ResultImc(props) {
+    return (
+        <View style={[styles.resultBox, { marginTop: props.marginTop }]}>
+            <View style={styles.texts}>
+                <Text style={styles.texto}>{props.messageResultImc}</Text>
+                <Text style={styles.texto}>{props.resultImc}</Text>
+            </View>
+            <Button
+                mode='contained'
+                style={styles.voltar}
+                onPress={() => props.setResultMaginTop(0, 200)}
+            >Voltar</Button>
         </View>
-
     );
 }
+
+const styles = StyleSheet.create({
+    resultBox: {
+        position: 'absolute',
+        zIndex: 2,
+        width: '80%',
+        height: '100%',
+        marginLeft: '10%',
+        backgroundColor: '#330077',
+        borderRadius: 10,
+        borderWidth: 3,
+        borderColor: '#77FF77',
+        display: 'flex',
+        justifyContent: 'space-between',
+        overflow: 'hidden',
+        // marginTop: '1000%',
+    },
+    texto: {
+        fontSize: 20,
+        textAlign: 'center',
+        // marginTop: 20,
+        color: '#FFF',
+        fontFamily: 'calibri',
+        fontWeight: '800'
+    },
+    voltar: {
+        borderBottomLeftRadius: 0,
+        borderBottomRightRadius: 0
+    },
+    texts: {
+        marginTop: '3%'
+    }
+});
--- src/components/Form/index.js
+++ src/components/Form/index.js
@@ -1,92 +1,185 @@
 import React, { useState } from "react";
-import { TextInput, View, Text, Button, Touchable, StyleSheet } from "react-native";
-import { TouchableOpacity } from "react-native-gesture-handler";
+import { View, Text, StyleSheet, Dimensions, ScrollView, KeyboardAvoidingView, Image } from "react-native";
 import ResultImc from "./ResultImc";
+import { Button, TextInput } from "react-native-paper";
+import { LinearGradient } from 'expo-linear-gradient';
 
-export default function Form(){
+export default function Form() {
+    const [altura, setAltura] = useState(null)
+    const [peso, setPeso] = useState(null)
+    const [messageImc, setMessageImc] = useState(null)
+    const [imc, setImc] = useState(null)
+    const [textButton, setTextButton] = useState("Calcular")
+    const screenWidth = Dimensions.get('window').width;
+    const screenHeight = Dimensions.get('window').height;
+    const [resultMaginTop, setResultMaginTop] = useState('200%')
 
-const [altura, setAltura] = useState(null)
-const [peso, setPeso] = useState(null)
-const [messageImc, setMessageImc] = useState("preencha o peso e altura")
-const [imc, setImc] = useState(null)
-const [textButton, setTextButton] = useState("Calcular")
-
-function validationImc(){
-    const imc = (peso/(altura*altura)).toFixed(2);
-    setImc(imc);
-
-    if(peso != null && altura != null){
-        if (imc <= 18.5){
-            setMessageImc("Abaixo do peso!")
-            setAltura('')
-            setPeso('')
-        } else if(imc > 18.5 && imc <= 25){
-            setMessageImc("Peso normal!")
-            setAltura('')
-            setPeso('')
-        } else if(imc > 25 && imc <= 30){
-            setMessageImc("Sobrepeso")
-            setAltura('')
-            setPeso('')
-        } else if(imc > 30 && imc <= 35){
-            setMessageImc("Obesidade grau I")
-            setAltura('')
-            setPeso('')
-        } else if(imc > 35 && imc <= 40){
-            setMessageImc("Obesidade grau II")
-            setAltura('')
-            setPeso('')
-        } else {
-            setMessageImc("Obesidade grau III")
-            setAltura('')
-            setPeso('')
-        }
-        setTextButton("Calcular novamente")
-        return
+    function animateResultBox(from, to) {
+        const speed = 10;
+        var value = from;
+        const lerpInterval = setInterval(() => {
+            if (from > to) {
+                if (value <= to) clearInterval(lerpInterval);
+                setResultMaginTop(value + '%')
+                value -= speed
+            } else {
+                if (value >= to) clearInterval(lerpInterval);
+                setResultMaginTop(value + '%')
+                value += speed
+            }
+        }, 1);
     }
-    setImc(null)
-    setTextButton("Calcular")
-    setMessageImc("preencha o peso e altura")
 
-}
-    return(
-        <View>
-            <View>
-                <Text>Altura</Text>
-                <TextInput
-                 onChangeText={setAltura}
-                 value={altura}
-                 placeholder="Ex. 1.75"
-                 keyboardType="numeric"
-                />
-                
-                <Text>Peso</Text>
-                <TextInput
-                 onChangeText={setPeso}
-                 value={peso}
-                 placeholder="Ex. 70.67"
-                 keyboardType="numeric"
-                />
+    function validationImc() {
+        const imc = (peso / (altura * altura)).toFixed(2);
+        setImc(imc);
+        animateResultBox(200, 0);
 
-                <ResultImc messageResultImc = {messageImc} resultImc = {imc}/>
+        if (peso != null && altura != null) {
+            if (imc <= 18.5) {
+                setMessageImc("Abaixo do peso!")
+                setAltura('')
+                setPeso('')
+            } else if (imc > 18.5 && imc <= 25) {
+                setMessageImc("Peso normal!")
+                setAltura('')
+                setPeso('')
+            } else if (imc > 25 && imc <= 30) {
+                setMessageImc("Sobrepeso")
+                setAltura('')
+                setPeso('')
+            } else if (imc > 30 && imc <= 35) {
+                setMessageImc("Obesidade grau I")
+                setAltura('')
+                setPeso('')
+            } else if (imc > 35 && imc <= 40) {
+                setMessageImc("Obesidade grau II")
+                setAltura('')
+                setPeso('')
+            } else {
+                setMessageImc("Obesidade grau III")
+                setAltura('')
+                setPeso('')
+            }
+            setTextButton("Calcular novamente")
+            return
+        }
+        setImc(null)
+        // setResultMaginTop('1000%')
+        setTextButton("Calcular")
+        setMessageImc("preencha o peso e altura")
+    }
+    return (
+        <View style={[styles.container, { width: screenWidth, height: screenHeight }]} >
 
-                <TouchableOpacity
-                 style={styles.estilos}   
-                 onPress={() => validationImc()}
-                >{textButton}</TouchableOpacity>
-            </View>
-            
-            
-        </View>
+            <KeyboardAvoidingView behavior="padding" style={{ flex: 1 }}>
+                <ScrollView keyboardShouldPersistTaps="always">
+                    <View style={[styles.app, { width: screenWidth, height: screenHeight }]}>
+                        <LinearGradient
+                            // Background Linear Gradient
+                            colors={['#00d4ff', '#2b259a']}
+                            start={{ x: 0, y: 0 }}
+                            end={{ x: 1, y: 1 }}
+                            style={styles.background}
+                        />
+                        <Image style={styles.imagem} source={require('../../../assets/calculo-imc.png')}></Image>
+                        <Text style={styles.titulo}>CALCULAR IMC</Text>
+                        <View style={styles.inputArea}>
+                            <TextInput
+                                style={styles.caixaAltura}
+                                label={'Altura'}
+                                // mode="outlined"
+                                onChangeText={setAltura}
+                                value={altura}
+                                placeholder="Ex. 1.75 Metros"
+                                keyboardType="numeric"
+                            />
+                            <TextInput
+                                style={styles.caixaPeso}
+                                label={'Peso'}
+                                // mode="outlined"
+                                onChangeText={setPeso}
+                                value={peso}
+                                placeholder="Ex. 70.67 Kg"
+                                keyboardType="numeric"
+                            />
+                            <Button
+                                mode='contained'
+                                style={styles.botaoCalcular}
+                                onPress={() => validationImc()}
+                            >{textButton}</Button>
+                            <ResultImc messageResultImc={messageImc} resultImc={imc} marginTop={resultMaginTop} setResultMaginTop={animateResultBox} />
+                        </View>
+                        
+                    </View>
+                </ScrollView>
+            </KeyboardAvoidingView>
+        </View >
     );
 }
 
 
 const styles = StyleSheet.create({
-    estilos:{
+    container: {
+        backgroundColor: '#fbeed3',
+        display: 'flex',
+        width: '100%',
+        height: '100%'
+
+    },
+    app: {
+        position: 'absolute',
+        display: 'flex',
+        justifyContent: "space-evenly"
+    },
+    background: {
+        position: 'absolute',
+        width: '100%',
+        height: '100%'
+    },
+    estilos: {
         backgroundColor: 'blue',
         color: 'red',
-    
+    },
+    caixaAltura: {
+        marginLeft: '10%',
+        marginRight: '10%',
+        // marginTop: '15%'
+    },
+    caixaPeso: {
+        // marginTop: '5%',
+        marginLeft: '10%',
+        marginRight: '10%',
+    },
+    botaoCalcular: {
+        // marginTop: '10%',
+        marginLeft: '25%',
+        marginRight: '25%',
+    },
+    imagem: {
+        width: 150,
+        height: 150,
+        // marginTop: 50,
+        alignItems: 'center'
+    },
+    titulo: {
+        fontSize: 35,
+        textAlign: 'center',
+        // marginTop: 20,
+        color: '#fff',
+        fontFamily: 'calibri',
+        fontWeight: '800',
+        zIndex: 1,
+    },
+    imagem: {
+        alignSelf: 'center',
+        width: 200,
+        height: 200,
+    },
+    inputArea: {
+        height: '30%',
+        display: 'flex',
+        justifyContent: 'space-evenly',
     }
+
 });
-  
